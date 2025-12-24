970fc28a734bb6d01e33d68575e4682d19efbfea
Deepak <appudeepak944@gmail.com>
2025-12-24 12:54:11 +0530

    Remove backend config.py from version control

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = "secret-key"
-    JWT_SECRET_KEY = "jwt-secret-key"
-
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:9353046405@localhost:9353/online_course_db"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
